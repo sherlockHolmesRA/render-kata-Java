08ac539788f2d65f1508755e3ca7cefc41a84db0
Rihab ATTIG <sherlockHolmesRA@users.noreply.github.com>
2022-01-31 02:45:31 +0100

    third rover commit

--- src/main/java/com/kata/rover/Rover.java
+++ src/main/java/com/kata/rover/Rover.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.Reader;
 import java.net.MalformedURLException;
-import java.util.EnumMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -18,73 +17,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Rover {
 
-    static final EnumMap<Direction, Direction> LEFT = new EnumMap<>(Direction.class);
-    static {
-        LEFT.put(Direction.N, Direction.W);
-        LEFT.put(Direction.W, Direction.S);
-        LEFT.put(Direction.S, Direction.E);
-        LEFT.put(Direction.E, Direction.N);
-    }
-
 	public static void RoverRunning(File file, PrintStream output) throws MalformedURLException, IOException {
-
-		// Stream Parsing.
 
 		Reader reader = new FileReader(file);
 
 		BufferedReader bReader = new BufferedReader(reader);
 
 		List<String> lines = bReader.lines().collect(Collectors.toList());
-
-		int startRover = 1;
-		int lineInput = 2;
-
-		int roverPosition = 0;
-		int mouvement = 1;
-
-		int nextMouvement = 1;
 		
-		for (int index = startRover; index < lines.size(); index += lineInput) {
+		for (int index = 1; index < lines.size(); index += 2) {
 
-            while (! lines.get(mouvement + index).isEmpty()) {
+            while (! lines.get(index + 1).isEmpty()) {
 
-                {
-                    String position = lines.get(roverPosition + index);
-                    String instructions = lines.get(mouvement + index);
+                String position = lines.get(index);
+				String instructions = lines.get(index + 1);
 
-                    if ("1 3 N".equals(position) && "".equals(instructions)) {
-                        lines.set(roverPosition + index, "1 3 N");
-                        lines.set(mouvement + index, "");
-                        break;
-                    }
-                    if ("5 3 E".equals(position) && "RMMRMRRM".equals(instructions)) {
-                        lines.set(roverPosition + index, "5 1 E");
-                        lines.set(mouvement + index, "");
-                        break;
-                    }
-                }
-
-                String position = lines.get(roverPosition + index);
-                String instructions = lines.get(mouvement + index);
-
-                final String currentLocation = position.substring(0, position.length()-2);
+                final String currentLocation = position.substring(0, position.length()-1);
                 final String startHeading = position.substring(position.length() - 1);
                 final Direction currentHeading = Direction.valueOf(startHeading);
 
                 final String currentInstruction = instructions.substring(0, 1);
-                final String remainingInstructions = instructions.substring(nextMouvement);
-                lines.set(mouvement + index, remainingInstructions);
+                final String remainingInstructions = instructions.substring(1);
+                lines.set(index + 1, remainingInstructions);
 
-                if ("L".equals(currentInstruction)) {
-                    String endHeading = LEFT.get(currentHeading).name();
-                    String endPosition = currentLocation + " " + endHeading;
-                    lines.set(roverPosition + index, endPosition);
+                String endHeading = currentHeading.name();
+                String endPosition = currentLocation + " " + endHeading;
+                
+                if ("R".equals(currentInstruction)) {
+                    lines.set(index + 1, endPosition);
                 }
 
+                
                 if ("M".equals(currentInstruction)) {
                     String[] rawCoordinates = currentLocation.split(" ");
                     int xPos = Integer.parseInt(rawCoordinates[0]);
-                    int yPos = Integer.parseInt(rawCoordinates[1]);
+                    int yPos = Integer.parseInt(rawCoordinates[0]);
 
                     if (Direction.W.equals(currentHeading)) {
                         xPos -= 1;
@@ -103,13 +70,10 @@ public class Rover {
                     }
 
                     String endLocation = xPos + " " + yPos;
-                    lines.set(roverPosition + index, endLocation + " " + currentHeading.name());
+                    lines.set(index, endLocation + " " + currentHeading.name());
                 }
             }
 		}	
-		
-		
-		
 
 		IntStream.range(0, lines.size()).filter(n -> (n % 2) == 1).mapToObj(n -> lines.get(n)).forEach(output::println);
 
--- src/test/java/com/kata/rover/RoverTests.java
+++ src/test/java/com/kata/rover/RoverTests.java
@@ -1,6 +1,5 @@
 package com.kata.rover;
 
-import org.junit.*;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -10,14 +9,10 @@ import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-@RunWith(Parameterized.class)
 class RoverTests {
 	
 	@Test
@@ -36,20 +31,6 @@ class RoverTests {
         check("5 5\n1 2 N\n\n3 3 E\n\n", "1 2 N\n3 3 E\n");
     }
 
-    @Parameters
-    Object[][] simplePrograms () {
-        return new Object[][] {
-                {"1 2 N", "L", "1 2 W"},
-                {"1 2 W", "L", "1 2 S"},
-                {"1 2 S", "L", "1 2 E"},
-                {"1 2 E", "L", "1 2 N"},
-                {"1 2 W", "M", "0 2 W"},
-                {"1 1 W", "L", "1 1 S"},
-                {"1 1 S", "M", "1 0 S"},
-                {"0 1 E", "M", "1 1 E"},
-                {"1 1 N", "M", "1 2 N"},
-        } ;
-    }
     
     @Test
     public void testSimpleProgram(String start, String instructions, String end) throws IOException {
